7395dcfeb3bc3f9d72b7ab71ebdfa946ed4d7e0f
Ting Gong <designer9418@icloud.com>
2019-08-15 19:45:33 +0800

    feat: support for generating iOS frameworks

---
+++ ios/EmbeddedReactNativeKit/EmbeddedReactNativeKit.h
@@ -0,0 +1,19 @@
+//
+//  EmbeddedReactNativeKit.h
+//  EmbeddedReactNativeKit
+//
+//  Created by Ting Gong on 2019/8/15.
+//  Copyright © 2019 650 Industries, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmbeddedReactNativeKit.
+FOUNDATION_EXPORT double EmbeddedReactNativeKitVersionNumber;
+
+//! Project version string for EmbeddedReactNativeKit.
+FOUNDATION_EXPORT const unsigned char EmbeddedReactNativeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmbeddedReactNativeKit/PublicHeader.h>
+
+
